528f99647028602b19d3ebd40b2fb390527d6957
shibly07 <shibly663@gmail.com>
2023-12-26 15:19:42 -0500

    Added email validation library

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
-const cors = require('cors')
+const cors = require("cors");
 const connectDb = require("./config/dbConnection");
 
 const userRoutes = require("./routes/userRoutes");
@@ -9,7 +9,7 @@ const errorHandler = require("./middleware/errorHandler");
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use("/api/user", userRoutes);
 app.use(errorHandler);
@@ -17,4 +17,8 @@ app.use(errorHandler);
 connectDb();
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.json({ title: "working", text: "hello" });
 });
