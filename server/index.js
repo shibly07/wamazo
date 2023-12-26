7043520818b97caf77f39315a7159d30afc36977
shibly07 <shibly663@gmail.com>
2023-12-26 13:35:43 -0500

    Added cors for client accessibility

--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const cors = require('cors')
 const connectDb = require("./config/dbConnection");
 
 const userRoutes = require("./routes/userRoutes");
 const errorHandler = require("./middleware/errorHandler");
 
-const app = express();
 const port = process.env.PORT || 3000;
+const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use("/api/user", userRoutes);
 app.use(errorHandler);
